bac9609f501860b885b22fa159999660f32bc61c
Zoia Krasnickia <zoya.ivanchuk@chat2desk.com>
2026-05-25 02:43:00 +0300

    fix:update structure

--- loops_practice.py
+++ loops_practice.py
@@ -1,28 +1,10 @@
 class Practice:
-    def list_numbers(self):
-        start = 1
-        end = 8
-        limit = 5
-
-        numbers = list(range(start, end))
-        for n in numbers:
-            if n == limit:
-                break
-            print(n)
-
 
     def list_words(self):
         limit = 10
         words = [f"str{i}" for i in range(limit)]
         for word in words:
             print(word)
-
-
-object_for_numbers = Practice()
-print("lesson1")
-object_for_numbers.list_numbers()
-
-print("-"*200)
 
 object_for_words = Practice()
 print("lesson2")
